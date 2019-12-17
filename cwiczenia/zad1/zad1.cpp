56e64aeb56499a5cf538208d1444eb6236bed294
dominikmomot@gmail.com <dominikmomot@gmail.com>
2019-12-17 16:47:02 +0100

    Zadanie pierwsze

---
+++ cwiczenia/zad1/zad1.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include "Cow.h"
+
+int main()
+{
+    {
+        Cow bazowy;
+        bazowy.show();
+        Cow jeden("Dominik Momot", "Programowanie", 30);
+        Cow trzy("aaa", "bbb", 30);
+        jeden.show();
+        Cow dwa = jeden;
+        dwa.show();
+        dwa = trzy;
+        dwa.show();
+
+    }
+    
+    
+}
+
