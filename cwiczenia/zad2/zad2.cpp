995ef4d26ba2a71546c0df1b867f4b0f42240356
dominikmomot@gmail.com <dominikmomot@gmail.com>
2019-12-17 18:17:15 +0100

    Zad3

--- cwiczenia/zad2/zad2.cpp
+++ cwiczenia/zad2/zad2.cpp
@@ -1,8 +1,19 @@
-﻿
-
-#include <iostream>
-
+﻿#include <iostream>
+#include "../Stringbad/Stringbad.cpp"
+#include "../Stringbad/Stringbad.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    Stringbad temp = "Dominik Momot";
+    Stringbad temp2 = " Programista";
+    Stringbad temp3;
+    temp3 = temp + temp2;
+    std::cout << temp3 << std::endl;
+    temp.stringlow();
+    std::cout << temp << std::endl;
+    temp.stringupper();
+    std::cout << temp << std::endl;
+    int number;
+    number = temp.howmany('M');
+    std::cout << number << std::endl;
 }
